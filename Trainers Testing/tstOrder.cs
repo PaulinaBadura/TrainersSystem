2023bce3dc96eaa499a7772fdb681f42fb5796a4
P2539038 <P2539038@my365.dmu.ac.uk>
2021-02-09 20:39:50 +0000

    Added testing and class for order management

---
+++ Trainers Testing/tstOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainersClasses;
+
+namespace Trainers_Testing
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //test to see if it exists
+            Assert.IsNotNull(AnOrder);
+
+        }
+    }
+}
